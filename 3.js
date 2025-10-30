c6762495eb09416776267fd7ab6d53f861e99358
l7.matniyozovv <matniyozovw.o10@gmail.com>
2025-10-30 18:39:05 +0500

    Add typing animation for various roles

---
+++ 3.js
@@ -0,0 +1,7 @@
+/* ===== typing animation ===== */
+var typed = new Typed(".typing", {
+  strings: ["Frontend Devoloper", "Web Designer", "Graphic Designer", ""],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
